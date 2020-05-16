495bade172e51cee2ce4ec52fd519de275774a17
Isaias Nascimento <isaias@webeasy.com.br>
2020-05-16 14:44:45 -0300

    componente item refeição

--- lib/screens/categories_meals_sreen.dart
+++ lib/screens/categories_meals_sreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals/components/meal_item.dart';
+import 'package:meals/data/dumy_data.dart';
 import 'package:meals/models/category.dart';
 
 class CategoriesMealScreen extends StatelessWidget {
@@ -12,13 +14,19 @@ class CategoriesMealScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context).settings.arguments as Category;
 
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(category.title),
       ),
-      body: Center(
-        child: Text('Receitas por categoria ${category.title}'),
-      ),
+      body: ListView.builder(
+          itemCount: categoryMeals.length,
+          itemBuilder: (ctx, index) {
+            return MealItem(categoryMeals[index]);
+          }),
     );
   }
 }
