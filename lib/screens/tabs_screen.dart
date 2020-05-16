a5e685e8eae2cfe200e9c9c8b2f24936b7e1605f
Isaias Nascimento <isaias@webeasy.com.br>
2020-05-16 16:34:31 -0300

    inclusão de Drawer

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals/components/main_drawer.dart';
 import 'package:meals/screens/categories_screen.dart';
 import 'package:meals/screens/favorite_screen.dart';
 
@@ -10,8 +11,8 @@ class TabScreen extends StatefulWidget {
 class _TabScreenState extends State<TabScreen> {
   int _selectedScreenIndex = 0;
   final List<Map<String, Object>> _screens = [
-    {'title':'Lista de Categorias','screen': CategoriesScreen()},
-    {'title':'Meus Favoritos','screen': FavoriteScreen()}
+    {'title': 'Lista de Categorias', 'screen': CategoriesScreen()},
+    {'title': 'Meus Favoritos', 'screen': FavoriteScreen()}
   ];
 
   _selectScreen(int index) {
@@ -26,6 +27,7 @@ class _TabScreenState extends State<TabScreen> {
       appBar: AppBar(
         title: Text(_screens[_selectedScreenIndex]['title']),
       ),
+      drawer: MainDrawer(),
       body: _screens[_selectedScreenIndex]['screen'],
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectScreen,
